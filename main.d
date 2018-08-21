c56815051a1acd3f9642ece74f4d6ece137b418f
Gia Duy Le Tran <GiaDuyLeTran@users.noreply.github.com>
2018-08-21 16:44:10 +0700

    Add files via upload

---
+++ main.d
@@ -0,0 +1,4 @@
+.\main.o: main.c
+.\main.o: H:\Embedded System\Edx course\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\main.o: uart.h
+.\main.o: TExaS.h
---
+++ uart.d
@@ -0,0 +1,3 @@
+.\uart.o: UART.c
+.\uart.o: UART.h
+.\uart.o: H:\Embedded System\Edx course\Keil\ARM\ARMCC\bin\..\include\stdio.h
